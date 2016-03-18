74f52970d9f7650cf69b51941cdf02f735fbb9fe
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2016-03-18 18:29:11 +0000

    isolate: add options, retrieve metadata

--- camisole/isolate.py
+++ camisole/isolate.py
@@ -19,36 +19,70 @@
 import asyncio
 import itertools
 import pathlib
+import tempfile
 
 async def communicate(self, cmdline, data=None, **kwargs):
     proc = await asyncio.create_subprocess_exec(
         *cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT, **kwargs)
+        stderr=subprocess.PIPE, **kwargs)
     stdout, stderr = await proc.communicate(data)
     retcode = await proc.wait()
     return retcode, stdout, stderr
 
 
 class IsolatorFactory:
-    def __init__(self, max_box_id=100):
+    def __init__(self, max_box_id=99):
         self.max_box_id = max_box_id
-        self.last_box_id = 0
+        self.available_box_id = set(range(max_box_id + 1))
 
     def __call__(self, *args, **kwargs):
-        self.last_box_id = (self.last_box_id + 1) % self.max_box_id
-        return Isolator(self.last_box_id, *args, **kwargs)
+        try:
+            curr_id = self.available_box_id.pop()
+        except KeyError:
+            raise RuntimeError("No isolate box ID available.")
+        else:
+            return Isolator(curr_id, *args, restore_id_cb=self.restore_id,
+                            **kwargs)
+
+    def restore_id(self, box_id):
+        if box_id not in range(max_box_id + 1):
+            raise RuntimeError("Trying to restore box id {} outside of id "
+                               "range.".format(box_id))
+        self.available_box_id.add(box_id)
 
 
 class Isolator:
-    def __init__(self, box_id, time_limit=None,
-                 mem_limit=None, allowed_dirs=None, processes=1):
+    def __init__(self, box_id, time_limit=None, wall_time_limit=None,
+                 mem_limit=None, fsize_limit=None,
+                 allowed_dirs=None, processes=1, file_restore_id_cb=None):
         self.box_id = box_id
         self.time_limit = time_limit
+        self.wall_time_limit = wall_time_limit
         self.mem_limit = mem_limit
+        self.fsize_limit = fsize_limit
         self.allowed_dirs = allowed_dirs if allowed_dirs is not None else []
         self.processes = processes
         self.box_path = None
+        self.restore_id_cb = restore_id_cb
         self.cmd_base = ['isolate', '--box-id', str(box_id), '--cg']
+
+        # Directory containing all the info of the program
+        self.info_dir = tempfile.TemporaryDirectory()
+        self.info_dir_path = pathlib.Path(self.info_dir)
+        self.info_stdout = self.info_dir_path / 'stdout'
+        self.info_stderr = self.info_dir_path / 'stderr'
+        self.info_meta = self.info_dir_path / 'meta'
+
+        # Cache the result of the program
+        self._stdout = None
+        self._stderr = None
+        self._meta = None
+
+        # Result of the isolate binary
+        self.isolate_retcode = None
+        self.isolate_stdout = None
+        self.isolate_stderr = None
+
 
     async def init(self):
         cmd_init = self.cmd_base + ['--init']
@@ -59,6 +93,8 @@ class Isolator:
     async def cleanup(self):
         cmd_cleanup = self.cmd_base + ['--cleanup']
         await communicate(cmd_init)
+        if self.restore_id_cb is not None:
+            self.restore_id_cb(self.box_id)
 
     async def run(self, cmdline, data=None, **kwargs):
         cmd_run = self.cmd_base
@@ -67,17 +103,42 @@ class Isolator:
         if self.mem_limit is not None:
             cmd_run += ['--mem', str(self.mem_limit)]
         if self.time_limit is not None:
-            cmd_run += ['--wall-time', str(self.time_limit)]
+            cmd_run += ['--time', str(self.time_limit)]
+        if self.wall_time_limit is not None:
+            cmd_run += ['--wall-time', str(self.wall_time_limit)]
+        if self.fsize_limit is not None:
+            cmd_run += ['--fsize', str(self.fsize_limit)]
 
         cmd_run += [
-            '--full-env',
+            '--meta={}'.format(self.info_meta),
+            '--stdout={}'.format(self.info_stdout),
+            '--stderr={}'.format(self.info_stderr),
             '--processes={}'.format(str(processes)),
             '--run', '--'
         ]
         cmd_run += cmdline
 
-        retcode, stdout, stdout = await communicate(cmd_run, data=data, **kwargs)
-        return retcode, exitcode, stdout
+        self.isolate_retcode, self.isolate_stdout, self.isolate_stderr = (
+            await communicate(cmd_run, data=data, **kwargs))
+
+    @property
+    def meta(self):
+        if self._meta is None:
+            meta_content = (l.strip() for l in self.info_meta.open())
+            self._meta = dict(l.split(':') for l in meta_content if l)
+        return self._meta
+
+    @property
+    def stdout(self):
+        if self._stdout is None:
+            self._stdout = self.info_stdout.open().read()
+        return self._stdout
+
+    @property
+    def stderr(self):
+        if self._stderr is None:
+            self._stderr = self.info_stderr.open().read()
+        return self._stderr
 
 
 get_isolator = IsolatorFactory()
