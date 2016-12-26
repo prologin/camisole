ea9e996e67efb8a28919a7ee849a1172a09aed2a
Alexandre Macabies <web+git@zopieux.com>
2016-12-26 18:51:29 +0100

    __main__: add host/port/logging-level argparse options

--- camisole/__main__.py
+++ camisole/__main__.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
+    import argparse
     import camisole.http
-    camisole.http.run()
+    import logging
+
+    parser = argparse.ArgumentParser(
+        description="asyncio-based source compiler and test runner",
+        add_help=False)
+    parser.add_argument('-h', '--host', default='0.0.0.0')
+    parser.add_argument('-p', '--port', type=int, default=8080)
+    parser.add_argument(
+        '-l',
+        '--logging',
+        default='error',
+        choices=[l.lower() for l in logging._nameToLevel],
+        help="logging level")
+    parser.add_argument('--help', action='help')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.logging.upper())
+    camisole.http.run(host=args.host, port=args.port)
