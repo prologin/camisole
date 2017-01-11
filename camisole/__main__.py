5e3c99a2d7edbf947c3f2be75f94539d6bb5d11c
Alexandre Macabies <web+git@zopieux.com>
2017-01-11 20:10:34 +0100

    main: add 'languages' command to test working languages; fix #11

--- camisole/__main__.py
+++ camisole/__main__.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
 
+async def print_working_languages():
+    from camisole.languages import all
+    from camisole.ref import test
+    import sys
+
+    use_color = sys.stdout.isatty()
+    status = {True: 'OK', False: 'FAIL'}
+    colors = {True: 32, False: 31}  # green, red
+
+    max_length = max(map(len, all())) + 2
+    for lang_name in sorted(all()):
+        ok, result = await test(lang_name)
+        ok = (f'\x1B[{colors[ok]}m{status[ok]}\033[0m' if use_color
+              else status[ok])
+        print(f'{lang_name + " ":.<{max_length}} {ok}', flush=True)
+
+
 if __name__ == '__main__':
     import argparse
+    import asyncio
     import camisole.http
     import logging
 
@@ -16,8 +34,18 @@ if __name__ == '__main__':
         default='error',
         choices=[l.lower() for l in logging._nameToLevel],
         help="logging level")
+    parser.add_argument(
+        'command',
+        nargs='?',
+        default='serve',
+        choices=['serve', 'languages'])
     parser.add_argument('--help', action='help')
     args = parser.parse_args()
 
     logging.basicConfig(level=args.logging.upper())
-    camisole.http.run(host=args.host, port=args.port)
+    loop = asyncio.get_event_loop()
+
+    if args.command == 'languages':
+        loop.run_until_complete(print_working_languages())
+    elif args.command == 'serve':
+        camisole.http.run(host=args.host, port=args.port)
