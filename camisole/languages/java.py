01391416dc1c67cc80dadddc18e0df7d14db9b16
Alexandre Macabies <web+git@zopieux.com>
2017-01-17 22:02:06 +0100

    languages: java: don't rely on javac output, "parse" the source instead

--- camisole/languages/java.py
+++ camisole/languages/java.py
@@ -3,21 +3,20 @@ from pathlib import Path
 
 from camisole.models import Lang
 
+RE_CLASS_DEF = re.compile(r'class\s+([^\s\{<]+)')
+
 
 class Java(Lang):
     source_ext = '.java'
+    compiled_ext = '.class'
     compiler = '/usr/bin/javac'
-    # verbose, so that class names can be extracted from stderr
-    compile_opts = ['-verbose']
     interpreter = '/usr/bin/java'
     # /usr/lib/jvm/java-8-openjdk/jre/lib/amd64/jvm.cfg links to
     # /etc/java-8-openjdk/amd64/jvm.cfg
     allowed_dirs = ['/etc/java-8-openjdk']
     reference_source = r'''
-class Main
-{
-    public static void main(String args[])
-    {
+public class JavaReference {
+    public static void main(String args[]) {
         System.out.println(42);
     }
 }
@@ -27,23 +26,22 @@ class Main
         # javac has no output directive, file name is class name
         return []
 
-    def read_compiled(self, path, isolator):
-        # extract output class from javac stderr
-        fname_reg = re.compile(r'^\[wrote RegularFileObject\[(.+?)\]\]$')
-        for line in reversed(isolator.stderr.split('\n')):
-            match = fname_reg.match(line)
-            if match:
-                wd = Path(isolator.path)
-                fname = Path(match.group(1)).name
-                # hack: store java class name in this instance
-                self.java_class = Path(fname).stem
-                return super().read_compiled(str(wd / fname), isolator)
+    def source_filename(self):
+        # best-effort parsing of the class <foo> directive, class name is
+        # stored in self.java_class for later use
+        source = self.opts.get('source', '')
+        match = RE_CLASS_DEF.search(source)
+        # arbitrary fallback so we just get an error later
+        self.java_class = 'Main'
+        if match:
+            self.java_class = match.group(1)
+        return self.java_class + self.source_ext
+
+    def execute_filename(self):
+        return self.java_class + self.compiled_ext
 
     def execute_command(self, output):
         # foo/Bar.class is run with $ java -cp foo Bar
         return [self.interpreter,
                 '-cp', str(Path(self.filter_box_prefix(output)).parent),
                 self.java_class]
-
-    def execute_filename(self):
-        return self.java_class + '.class'
