f990253422ab75e4c35c2439851dc99ef34da716
Antoine Pietri <antoine.pietri@lrde.epita.fr>
2017-01-27 22:47:18 +0100

    languages: c++11 -> c++14

--- camisole/languages/cxx.py
+++ camisole/languages/cxx.py
@@ -4,7 +4,7 @@ from camisole.models import Lang
 class CXX(Lang, name="C++"):
     source_ext = '.cc'
     compiler = '/usr/bin/g++'
-    compile_opts = ['-std=c++11', '-Wall', '-Wextra', '-O2']
+    compile_opts = ['-std=c++14', '-Wall', '-Wextra', '-O2']
     reference_source = r'''
 #include <iostream>
 int main()
