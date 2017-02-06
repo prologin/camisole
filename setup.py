70e9c9b9d4252383dc85c1db2cec0470765e8ceb
Alexandre Macabies <web+git@zopieux.com>
2017-02-06 11:53:49 +0100

    setup.py: bump to 0.4; add pytest stuff

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='camisole',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     url='https://bitbucket.com/prologin/camisole',
     license='GPL',
     author='serialk, zopieux',
     description='An asyncio-based source compiler and test runner.',
     install_requires=['aiohttp'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-cov'],
+    test_suite='pytest',
     entry_points={
         'console_scripts': ['camisole = camisole.__main__:main'],
     },
