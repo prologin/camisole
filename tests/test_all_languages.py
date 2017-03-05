8584980fd995db9dd17d1a4cf1370e57d5a6223f
Alexandre Macabies <web+git@zopieux.com>
2017-03-05 21:29:27 +0100

    tests: all_languages: remove xfail()

--- tests/test_all_languages.py
+++ tests/test_all_languages.py
@@ -5,7 +5,7 @@ camisole.languages._import_builtins()
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("language", map(pytest.mark.xfail, camisole.languages.all()))
+@pytest.mark.parametrize("language", camisole.languages.all())
 async def test(language):
     lang_cls = camisole.languages.by_name(language)
     l = lang_cls({'source': lang_cls.reference_source, 'tests': [{}]})
