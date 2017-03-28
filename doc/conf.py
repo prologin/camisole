70251876bca0df810b9bf0d094f9eec529edc827
Antoine Pietri <antoine.pietri1@gmail.com>
2017-03-28 00:44:49 +0000

    doc: add logo and globaltoc sidebar

--- doc/conf.py
+++ doc/conf.py
@@ -102,6 +102,9 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 
+html_show_sourcelink = False
+html_sidebars = { '**': ['camisole.html', 'globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'], }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
