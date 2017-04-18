fdd3dc513ac021ac8597b84e48c455fd0109969c
multun <victor.collod@epita.fr>
2017-04-18 15:15:29 +0900

    langs: rust: add

--- camisole/languages/__init__.py
+++ camisole/languages/__init__.py
@@ -34,6 +34,7 @@ __all__ = [
     'php',
     'python',
     'ruby',
+    'rust',
     'scheme',
     'visualbasic',
 ]
---
+++ camisole/languages/rust.py
@@ -0,0 +1,10 @@
+from camisole.models import Lang, Program
+
+
+class Rust(Lang):
+    source_ext = '.rs'
+    compiler = Program('rustc', opts=['-W', 'warnings', '-O'])
+    reference_source = r'''\
+fn main() {
+    println!("Hello, world!");
+}'''
