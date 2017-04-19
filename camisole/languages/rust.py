9ccb83959c96b54c4af473bd4d881db8f235265d
Antoine Pietri <antoine.pietri1@gmail.com>
2017-04-19 09:10:43 +0000

    rust: fix reference source

--- camisole/languages/rust.py
+++ camisole/languages/rust.py
@@ -4,7 +4,7 @@ from camisole.models import Lang, Program
 class Rust(Lang):
     source_ext = '.rs'
     compiler = Program('rustc', opts=['-W', 'warnings', '-O'])
-    reference_source = r'''\
+    reference_source = r'''
 fn main() {
-    println!("Hello, world!");
+    println!("42");
 }'''
