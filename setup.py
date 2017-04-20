1fb111e1a0211b90371aabaa760d4cc7dfa718e3
Antoine Pietri <antoine.pietri1@gmail.com>
2017-04-20 02:06:24 +0000

    camisole 0.5

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ if sys.version_info < (3, 6):
 
 setup(
     name='camisole',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     url='https://github.com/prologin/camisole',
     license='GPL',
-    author='serialk, zopieux',
+    author='Antoine Pietri, Alexandre Macabies',
     description='An asyncio-based source compiler and test runner.',
     install_requires=['aiohttp'],
     setup_requires=['pytest-runner'],
