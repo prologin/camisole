03015ed5bbca98d9a954b95f13234c724ae4bf18
Antoine Pietri <antoine.pietri1@gmail.com>
2017-04-21 21:16:05 +0200

    deployment: add OVA upload script

---
+++ deployment/packer/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $( dirname $0 )
+
+latest=$( ls output-virtualbox-iso/*.ova | sort | tail -n1 )
+
+date=$( date --rfc-3339=date )
+relpath="camisole-$date.ova"
+path="/home/camisole/www/ova/$relpath"
+
+scp "$latest" "camisole@prologin.org:$path"
+
+ssh camisole@prologin.org chmod 644 "$path"
+ssh camisole@prologin.org ln -sf "./$relpath" /home/camisole/www/ova/camisole-latest.ova
