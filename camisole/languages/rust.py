4e33b1e83184116eeb368a44451fcaf696cf001e
Alexandre Macabies <web+git@zopieux.com>
2017-10-21 17:34:52 +0200

    languages: rust: reference source on a single line

--- camisole/languages/rust.py
+++ camisole/languages/rust.py
@@ -4,7 +4,4 @@ from camisole.models import Lang, Program
 class Rust(Lang):
     source_ext = '.rs'
     compiler = Program('rustc', opts=['-W', 'warnings', '-O'])
-    reference_source = r'''
-fn main() {
-    println!("42");
-}'''
+    reference_source = r'fn main() { println!("42"); }'
