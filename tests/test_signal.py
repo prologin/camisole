c36ea9c0b064b0e2e9ae1f0f52f4aacd8ba729ae
Alexandre Macabies <web+git@zopieux.com>
2017-10-23 19:52:42 +0200

    test: signal: remove buggy testcase while ioi/isolate#37 is discussed

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -5,7 +5,6 @@ from camisole.languages import by_name
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("source_and_signal", (
-    ('int a = 0; return 1 / a;', 4, "Illegal instruction"),
     ('volatile int a = 0; return 1 / a;', 8, "Floating point exception"),
 ))
 async def test_signal_message(source_and_signal):
