64f6f0cd8331b7dda085603839e36b579bdc2b4b
Antoine Pietri <antoine.pietri@prologin.org>
2018-02-06 16:33:19 +0000

    deployment: build_depends: remove AUR

--- deployment/pkg/camisole-languages/build_depends.py
+++ deployment/pkg/camisole-languages/build_depends.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 import sys
 from pathlib import Path
@@ -12,20 +13,10 @@ def by_name(name):
     return Lang._full_registry[name]
 
 
-# AUR has no pkgfile mechanism; we provide a manually maintained list of
-# binary -> package instead
-AUR_PROVIDES = {'vbnc': 'mono-basic',
-                'fsharpc': 'fsharp',
-                'esotope-bfc': 'esotope-bfc-git'}
-
 # java packages consist of symlinks to handle both Java 7 & 8, so we force
 # the version (8) here
 OVERWRITE = {by_name('java'): {'/usr/lib/jvm/java-8-openjdk/bin/java',
                                '/usr/lib/jvm/java-8-openjdk/bin/javac'}}
-
-
-def error(msg):
-    print("ERROR: " + msg, file=sys.stderr)
 
 
 def list_paths():
@@ -39,31 +30,13 @@ def list_paths():
             yield cls, Path(p.cmd)
 
 
-def check_pkg_provides(pkg, name):
-    try:
-        contents = subprocess.check_output(['pacman', '-Qql', pkg])
-    except subprocess.CalledProcessError:
-        error(f"AUR package {pkg} not installed, cannot check contents")
-        return False
-    for line in contents.decode().splitlines():
-        if Path(line.strip()).name == name:
-            return True
-    return False
-
-
 def get_package(binary):
-    pkg = None
     try:
         pkg = (subprocess.check_output(['pkgfile', '-qb', binary])
                .decode().split('\n')[0]) or None
-    except subprocess.CalledProcessError:
-        pass
-    if pkg:
         return pkg
-    if not pkg:
-        pkg = AUR_PROVIDES.get(binary.name)
-        if pkg and check_pkg_provides(pkg, binary.name):
-            return pkg
+    except subprocess.CalledProcessError:
+        return None
 
 
 if __name__ == '__main__':
@@ -71,7 +44,7 @@ if __name__ == '__main__':
     for lang, binary in list_paths():
         pkg = get_package(binary)
         if pkg is None:
-            error(f"no package for {binary}")
+            logging.error("no package for %s", binary)
             continue
         packages[pkg].add(lang)
     packages.pop('python', None)  # already a strong dependency
