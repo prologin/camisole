db9f2047aa61871f92b846d77ffb3e7a4eb3f96c
Matthieu Moatti <moatti_m@epita.fr>
2018-10-23 20:12:42 +0200

    languages: update g++ flag from c++14 to c++17

--- camisole/languages/cxx.py
+++ camisole/languages/cxx.py
@@ -3,7 +3,7 @@ from camisole.models import Lang, Program
 
 class CXX(Lang, name="C++"):
     source_ext = '.cc'
-    compiler = Program('g++', opts=['-std=c++14', '-Wall', '-Wextra', '-O2'])
+    compiler = Program('g++', opts=['-std=c++17', '-Wall', '-Wextra', '-O2'])
     reference_source = r'''
 #include <iostream>
 int main()
