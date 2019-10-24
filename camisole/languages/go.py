c100c44d26ddf2d8bcc9dc6c72a602d4ad613238
Léo PORTEMONT <leo.portemont@epita.fr>
2019-10-24 18:32:33 +0200

    go: set GOCACHE environment variable

--- camisole/languages/go.py
+++ camisole/languages/go.py
@@ -4,7 +4,8 @@ from camisole.models import Lang, Program
 class Go(Lang):
     source_ext = '.go'
     compiler = Program('go', opts=['build', '-buildmode=exe'],
-                       version_opt='version')
+                       version_opt='version',
+                       env={'GOCACHE':'/box/.gocache'})
     reference_source = r'''
 package main
 import "fmt"
