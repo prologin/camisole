579b721c79019884496ac0f5dfb919197d80cd1d
BlackBeans <adrien.mathieu.net@gmail.com>
2021-02-20 13:33:26 +0100

    languages: add LLVM asm support

---
+++ camisole/languages/llvmasm.py
@@ -0,0 +1,15 @@
+from camisole.models import Lang, Program
+
+class LLVMAsm(Lang):
+    source_ext = '.ll'
+    interpreter = Program('lli-8')
+    reference_source = """@str42 = internal constant [4 x i8] c"42\0A\00"
+
+declare i32 @printf(i8*, ...)
+
+define i32 @main(...) nounwind {
+entry:
+  %strptr = getelementptr [4 x i8], [4 x i8]* @str42, i32 0, i32 0
+  call i32 (i8*, ...) @printf( i8* %strptr ) nounwind
+  ret i32 0
+}"""
