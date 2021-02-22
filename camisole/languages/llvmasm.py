7d6cd364d0185e7316e6d72f2a0ad29ab0c36bcb
BlackBeans <adrien.mathieu.net@gmail.com>
2021-02-22 20:22:30 +0100

    languages: patched clang abusive warning for llvmasm

--- camisole/languages/llvmasm.py
+++ camisole/languages/llvmasm.py
@@ -2,8 +2,12 @@ from camisole.models import Lang, Program
 
 class LLVMAsm(Lang):
     source_ext = '.ll'
-    compiler = Program('clang-8')
-    interpreter = Program('lli-8')
+    compiler = Program('clang-8', opts=[ # Statically compile the program.
+        '-x', 'ir',                      # Force the target language to be LLVM AS.
+        '-Wno-override-module'           # Because the source file might lack the target module,
+                                         # we don't want clang to complain about it.
+    ])
+    interpreter = Program('lli-8')       # JIT compiler.
     reference_source = r"""
 @str42 = internal constant [4 x i8] c"42\0A\00"
 
