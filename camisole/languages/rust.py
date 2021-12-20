f0f3daf15586904a9f91f6cfa2f4c66f20be9bd5
elvyn <45512739+leElvyn@users.noreply.github.com>
2021-12-20 16:02:52 +0100

    rust: add optimization to args
    * Full optimization for the Rust compiler
    
    * Splitted arguments
    
    * Typo

--- camisole/languages/rust.py
+++ camisole/languages/rust.py
@@ -3,5 +3,5 @@ from camisole.models import Lang, Program
 
 class Rust(Lang):
     source_ext = '.rs'
-    compiler = Program('rustc', opts=['-W', 'warnings', '-O'])
+    compiler = Program('rustc', opts=['-W', 'warnings', '-C', 'opt-level=3'])
     reference_source = r'fn main() { println!("42"); }'
